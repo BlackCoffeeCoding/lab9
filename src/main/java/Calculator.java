d21a45b384ec5347d1a3f7fbc5b72b62ea561eed
Ursus <kvsolntsev@gmail.com>
2024-05-06 15:12:00 +0300

    try

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int dif(int a, int b){
+        return a-b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
+    public int times(int a, int b){
+        return a*b;
+    }
+    public int solver(){
+        int a=7;
+        int b=1;
+        return dif(a,b)+b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    Calculator calculator=new Calculator();
+
+    @Test
+    void add() {
+        assertEquals(0,calculator.add(-1,1));
+    }
+
+    @Test
+    void dif() {
+        //парам
+        assertEquals(-2,calculator.dif(-1,1));
+    }
+
+    @Test
+    void div() {
+        assertEquals(0,calculator.div(0,1));
+    }
+
+    @Test
+    void times() {
+        assertEquals(-1,calculator.times(-1,1));
+    }
+
+    @Test
+    void solver() {
+        assertEquals(7,calculator.solver());
+    }
+}
